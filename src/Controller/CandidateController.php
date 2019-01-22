267d2167ddd128d0fe6e4831c40da49ec77c4e8e
zozowy <aude.millequant@wanadoo.fr>
2019-01-22 21:32:18 +0100

    premiersjets

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,23 +2,42 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Repository\UserRepository;
+use App\Repository\VisitCardRepository;
 
 class CandidateController extends AbstractController
 {
     /**
-     * @Route("/candidate", name="candidate")
+     * @Route("/entreprise", name="entreprise_index")
      */
-    public function index(UserRepository $userRepo)
+    public function index(VisitCardRepository $visitCardRepo)
     {
-        $users= $userRepo->findAll();
+        
+        //$candidates= $isCandidateRepo->findAll();
+        $visitCards = $visitCardRepo->findAll();
+        //dd($visitCard);
         
         return $this->render('candidate/index.html.twig', [
-            'users'=>$users,
+            //'candidates'=>$candidates,
+            'visitCards'=>$visitCards
+        ]);
+    }
+
+     /**
+     * @Route("/entreprise/candidates", name="candidates_list")
+     */
+    public function showCandidate (VisitCardRepository $visitCardRepo)
+    {
+        
+        //$candidates= $isCandidateRepo->findAll();
+        $visitCards = $visitCardRepo->findAll();
+        //dd($visitCard);
+        
+        return $this->render('candidate/show.html.twig', [
+            //'candidates'=>$candidates,
+            'visitCards'=>$visitCards
         ]);
     }
 }
