16628eeddc540f3c50df0bbe5b69b6b752aaa6e1
zozowy <aude.millequant@wanadoo.fr>
2019-01-22 16:09:19 +0100

    1er tri

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 
 class CandidateController extends AbstractController
 {
     /**
      * @Route("/candidate", name="candidate")
      */
-    public function index()
+    public function index(UserRepository $userRepo)
     {
+        $users= $userRepo->findAll();
+        
         return $this->render('candidate/index.html.twig', [
-            'controller_name' => 'CandidateController',
+            'users'=>$users,
         ]);
     }
 }
