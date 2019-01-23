2f8f622bf63d8d3d4a390f465a3ce04b4ec67882
zozowy <aude.millequant@wanadoo.fr>
2019-01-23 15:24:42 +0100

    code en attente de merge pour evoluer

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -6,22 +6,27 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\VisitCardRepository;
+use App\Repository\ArticleRepository;
 
 class CandidateController extends AbstractController
 {
     /**
      * @Route("/candidates", name="candidates_index")
      */
-    public function index(VisitCardRepository $visitCardRepo)
+    public function index(VisitCardRepository $visitCardRepo, ArticleRepository $articleRepo)
     {
         
-        //$candidates= $isCandidateRepo->findAll();
+        
         $visitCards = $visitCardRepo->findAll();
+        $articles = $articleRepo->findAll();
         //dd($visitCard);
+        //dd($articles);
         
         return $this->render('candidate/index.html.twig', [
             //'candidates'=>$candidates,
-            'visitCards'=>$visitCards
+            'visitCards'=>$visitCards,
+            'articles'=>$articles
+
         ]);
     }
 
