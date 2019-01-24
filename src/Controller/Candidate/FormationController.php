fc8ec9d0cab71404b241ef419d35507b46607378
Rachel-ney <michel.rachel@hotmail.fr>
2019-01-24 15:17:49 +0100

    Controller + route + template pour sprint 2

---
+++ src/Controller/Candidate/FormationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Candidate;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/candidat/formation", name="formation_")
+ */
+class FormationController extends AbstractController
+{
+    /**
+     * @Route("/ajouter", name="add")
+     */
+    public function add()
+    {
+        return $this->render('candidate/profil/formation.html.twig', [
+            'controller_name' => 'FormationController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/modifier", name="edit")
+     */
+    public function edit()
+    {
+        return $this->render('candidate/profil/formation.html.twig', [
+            'controller_name' => 'FormationController',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/supprimer", name="delete")
+     */
+    public function delete()
+    {
+        return $this->redirectToRoute('candidate_profil');
+    }
+}
