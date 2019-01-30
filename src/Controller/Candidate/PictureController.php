b391188e07566715c362b3ef90f692c090b3946b
Cyriel Martin <cyriel.martin@gmail.com>
2019-01-30 15:29:07 +0100

    WIP

--- src/Controller/Candidate/PictureController.php
+++ src/Controller/Candidate/PictureController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Candidate;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\IsCandidate;
+use App\Form\IsCandidateType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/candidat/image", name="candidate_picture")
@@ -23,10 +27,23 @@ class PictureController extends AbstractController
     /**
      * @Route("/{id}/modifier", name="edit")
      */
-    public function edit()
-    {
+    public function edit(IsCandidate $isCandidate, Request $request, EntityManagerInterface $em)
+        {
+            $isCandidateForm = $this->createForm(IsCandidateType::class, $isCandidate);
+            $isCandidateForm->handleRequest($request);
+            if ($isCandidateForm->isSubmitted() && $isCandidateForm->isValid()) {
+                $em->persist($isCandidate);
+                $em->flush();
+                
+                $this->addFlash(
+                    'notice',
+                    'La photo a bien été modifiée'
+                );
+                return $this->redirectToRoute('about_edit', ['id' => $user->getId()]);
+            }
+    
         return $this->render('candidate/profile/picture.html.twig', [
-            'controller_name' => 'PictureController',
+            'isCandidateForm' => $isCandidateForm->createView(),
         ]);
     }
 
