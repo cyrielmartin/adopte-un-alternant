bce7791a0917581b2ed5df064573b335d301c8c9
Cyriel Martin <cyriel.martin@gmail.com>
2019-01-31 14:51:14 +0100

    Skills add, delete, edit ok

--- src/Controller/Candidate/SkillController.php
+++ src/Controller/Candidate/SkillController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Candidate;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Skill;
+use App\Form\SkillType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/candidat/competence", name="skill_")
@@ -13,28 +17,53 @@ class SkillController extends AbstractController
     /**
      * @Route("/ajouter", name="add")
      */
-    public function add()
+    public function add(Request $request, EntityManagerInterface $em)
     {
+        $skill = new Skill();
+        $form = $this->createForm(SkillType::class, $skill);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($skill);
+            $em->flush();
+            
+            return $this->redirectToRoute('candidate_profile');
+        }
+
         return $this->render('candidate/profile/skill.html.twig', [
-            'controller_name' => 'SkillController',
+            'form' => $form->createView(),
         ]);
     }
 
     /**
      * @Route("/{id}/modifier", name="edit")
      */
-    public function edit()
+
+    public function edit(Skill $skill, Request $request, EntityManagerInterface $em)
     {
-        return $this->render('candidate/profile/skill.html.twig', [
-            'controller_name' => 'SkillController',
-        ]);
+    $form = $this->createForm(SkillType::class, $skill);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($skill);
+        $em->flush();
+
+        return $this->redirectToRoute('candidate_profile');
     }
+
+    return $this->render('candidate/profile/skill.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
 
     /**
      * @Route("/{id}/supprimer", name="delete")
      */
-    public function delete()
+    public function delete(Skill $skill, EntityManagerInterface $em)
     {
+        $em->remove($skill);
+        $em->flush();
+        
         return $this->redirectToRoute('candidate_profile');
     }
 }
