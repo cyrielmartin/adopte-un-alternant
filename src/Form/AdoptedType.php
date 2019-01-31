8c9564829f26c23bfea2348cdfa2340e91f37891
Cyriel Martin <cyriel.martin@gmail.com>
2019-01-31 10:22:16 +0100

    ok

--- src/Form/AdoptedType.php
+++ src/Form/AdoptedType.php
@@ -13,7 +13,6 @@ class AdoptedType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            // ->add('about')
             ->add('adopted', ChoiceType::class, [
                 'label' => 'Avez-vous été adopté par une entreprise ?',
                 'help' => 'Ne cochez oui que si votre contrat d\'alternance a été signé',
