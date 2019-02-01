12d9eb4274dfaac8152d816c9b17e14bd0700989
Cyriel Martin <cyriel.martin@gmail.com>
2019-02-01 22:49:44 +0100

    Delete Skill WIP

--- src/Controller/Candidate/SkillController.php
+++ src/Controller/Candidate/SkillController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Candidate;
 use App\Entity\Skill;
 use App\Form\SkillType;
 use App\Entity\VisitCard;
+use App\Entity\IsCandidate;
 use App\Form\SkillVisitCardType;
 use App\Form\VisitCardSkillType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -43,4 +44,40 @@ class SkillController extends AbstractController
     ]);
     }
 
+    /**
+     * @Route("/{id}/supprimer", name="delete")
+     */
+
+    public function delete($id)
+    {
+        // je récupère le user
+        $user = $this->getUser();
+        // je récupère sa fiche candidat
+        $candidateRepo = $this->getDoctrine()->getRepository(IsCandidate::class);
+        $candidate = $candidateRepo->findOneBy(['user' => $user->getId()]);
+        // je récupère la carte de visite du candidat
+        $visitCardRepo = $this->getDoctrine()->getRepository(VisitCard::class);
+        $visitCard = $visitCardRepo->findOneBy(['isCandidate' => $candidate->getId()]);
+        // je récupère la compétence du candidat connecté, qui doit être supprimé
+        $skillRepo = $this->getDoctrine()->getRepository(Skill::class);
+      
+        $skill = $skillRepo->findOneBy(['id' => $id, 'visitCard' => $visitCard->getId()]);
+        
+        // si cette compétence appartient bien au candidat connecté
+        if(!empty($skill))
+        {
+            $em = $this->getDoctrine()->getManager();
+            // je le supprime
+            $em->remove($skill);
+            $em->flush();
+
+            $this->addFlash('success', 'Votre compétence a bien été supprimée.');
+        }
+        else
+        {
+            $this->addFlash('danger', 'Une erreur est survenue lors de la suppression.');
+        }
+        
+        return $this->redirectToRoute('candidate_profile');
+    }
 }
