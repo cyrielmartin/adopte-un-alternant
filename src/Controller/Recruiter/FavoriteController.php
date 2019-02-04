ea61cb6482e935a7dca4a0ce13476ed3f842c38f
zozowy <aude.millequant@wanadoo.fr>
2019-02-04 11:36:25 +0100

    gd nimporte quoi pour linstant

--- src/Controller/Recruiter/FavoriteController.php
+++ src/Controller/Recruiter/FavoriteController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Recruiter;
 
+use App\Entity\IsCandidate;
+use App\Entity\IsRecruiter;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\RecruiterFavoriteCandidateType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -13,7 +18,7 @@ class FavoriteController extends AbstractController
     /**
      * @Route("/{id}/ajouter", name="add")
      */
-    public function add($id)
+    public function add($id, Request $request, EntityManagerInterface $em)
     {
         /** 
          * Comme le bouton "ajouter aux favoris" se situe dans la page de profil publique d'un candidat, il faudra rediriger 
@@ -22,6 +27,45 @@ class FavoriteController extends AbstractController
          * Penser à vérifier que l'id est bien l'id d'un candidat existant avant de faire toute action
          * si id non existant : return $this->redirectToRoute('candidates_list');
          * */ 
+        dump($id);
+
+        $user = $this->getUser();
+        
+        dump($user);
+        // je récupère sa fiche recruteur
+        $recruiterRepo = $em->getRepository(IsRecruiter::class);
+        $recruteur= $recruiterRepo->findOneBy(['user' => $user->getId()]);
+        
+        //dd($recruteur);
+
+        //$favoriteCandidate = new IsRecruiter();
+
+        $form = $this->createForm(RecruiterFavoriteCandidateType::class, $recruteur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            //$favoriteCandidate = $form->getData();
+            //dd($favoriteCandidate);
+
+            // je vérifie que la ville existe
+            //$town = MobilityManager::isRealTown($mobility);
+            
+            // si la clef fail existe, l'api n'a renvoyé aucun résultat
+            // c'est donc un message d'erreur qui a été retourné
+            //if(isset($town['fail']))
+            //{
+            //    $this->addFlash('danger', $town['fail']);
+            //    return $this->redirectToRoute('mobility_add');
+            //}
+            //// sinon l'api a renvoyé un résultat
+            //// $town['success'] contient le tableau de réponse renvoyé par l'api
+            //$mobility = MobilityManager::recoverMobility($town, $em);
+            // je lie ma mobilité et ma carte de viste 
+            $recruter->addIsCandidate($id);
+            $em->persist($recruter);
+            $em->flush();
+        }
 
         return $this->redirectToRoute('candidates_one', ['id' => $id]);
     }
